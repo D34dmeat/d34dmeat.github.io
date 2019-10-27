e54b40d69e35b7cb1069b61ab40d80a99861e6ac
Mikael Edlund <d34dmeat@gmail.com>
2019-10-27 14:14:38 +0100

    Update post.php

--- php/post.php
+++ php/post.php
@@ -1,8 +1,8 @@
 if(!empty($_POST['data'])){
 $data = $_POST['data'];
-$fname = mktime() . ".yml";//generates random name
+$fname = "posting.yml";//generates random name = mktime() . "
 
-$file = fopen("_data/" .$fname, 'w');//creates new file
+$file = fopen("_data/" .$fname, 'rw');//creates new file
 fwrite($file, $data);
 fclose($file);
 }
